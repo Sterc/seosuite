417867ad58f22231b87cc4fa96e8183effb2b8cc
Joeke Kloosterman <joekekloosterman@gmail.com>
2016-10-07 13:17:41 +0200

    Add package transport zip

---
+++ _build/gpm_resolvers/gpm.resolve.tables.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Resolve creating db tables
+ *
+ * THIS RESOLVER IS AUTOMATICALLY GENERATED, NO CHANGES WILL APPLY
+ *
+ * @package buster404
+ * @subpackage build
+ */
+
+if ($object->xpdo) {
+    $modx =& $object->xpdo;
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            $modelPath = $modx->getOption('buster404.core_path', null, $modx->getOption('core_path') . 'components/buster404/') . 'model/';
+            
+            $modx->addPackage('buster404', $modelPath, null);
+
+
+            $manager = $modx->getManager();
+
+            $manager->createObjectContainer('Buster404Url');
+            $manager->createObjectContainer('Buster404Source');
+
+            break;
+    }
+}
+
+return true;
