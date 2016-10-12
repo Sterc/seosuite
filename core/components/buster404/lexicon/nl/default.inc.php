576d449ff785fbb1f85f47ce2750f0742db048e2
Sytske Haagsma <sytske@sterc.nl>
2016-10-12 13:33:21 +0200

    Added nl translations and changed some translations

---
+++ core/components/buster404/lexicon/nl/default.inc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Default Dutch Lexicon Entries for Buster404
+ *
+ * @package buster404
+ * @subpackage lexicon
+ */
+
+$_lang['buster404'] = '404 Buster';
+
+$_lang['buster404.menu.buster404'] = '404 Buster';
+$_lang['buster404.menu.buster404_desc'] = 'Repareer 404 not-found URLs.';
+
+$_lang['buster404.global.search'] = 'Zoeken';
+
+$_lang['buster404.url.urls'] = '404 URLs';
+$_lang['buster404.url.intro_msg'] = 'Beheer uw 404 URL\'s hier. Upload een csv bestand met je 
+404 URL\'s en de URL\'s worden automatisch omgeleid naar bestaande pagina\'s op uw site.';
+$_lang['buster404.url.import'] = 'Importeer bestand';
+
+$_lang['buster404.url.url'] = '404 URL';
+$_lang['buster404.url.solved'] = 'Opgelost';
+$_lang['buster404.url.position'] = 'Positie';
+$_lang['buster404.url.redirect_to'] = 'Redirect naar';
+$_lang['buster404.url.suggestions'] = 'Redirect suggesties';
+$_lang['buster404.url.find_suggestions'] = 'Vind suggesties';
+$_lang['buster404.url.update'] = 'Update URL';
+$_lang['buster404.url.remove'] = 'Verwijder URL';
+$_lang['buster404.url.remove_confirm'] = 'Weet u zeker dat u deze URL wil verwijderen?';
+
+$_lang['buster404.error.url_alreadyexists'] = 'Deze URL bestaat al.';
+$_lang['buster404.error.url_notfound'] = 'Item niet gevonden.';
+$_lang['buster404.err.item_name_ae'] = 'Item niet gevonden.';
+$_lang['buster404.error.url_notspecified'] = 'URL is niet gedefinieerd.';
+$_lang['buster404.err.item_name_ns'] = 'Waarde is niet gedefinieerd.';
+$_lang['buster404.error.url_remove'] = 'Er is een fout opgetreden tijdens het verwijderen van de URL.';
+$_lang['buster404.error.url_save'] = 'Er is een fout opgetreden tijdens het opslaan van de URL.';
+$_lang['buster404.error.emptyfile'] = 'Geen bestand opgegeven';
+$_lang['buster404.error.extension_notallowed'] = 'Bestandstype niet toegestaan. Alleen csv bestanden zijn toegestaan.';
