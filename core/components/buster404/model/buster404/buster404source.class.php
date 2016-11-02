b548f19a9a0d5df87a528b8eebe0b0e5ead59ff0
Joeke Kloosterman <joekekloosterman@gmail.com>
2016-11-02 22:00:11 +0100

    Remove the Buster404Source class and mysql files

--- core/components/buster404/model/buster404/buster404source.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * @package buster404
- */
-class Buster404Source extends xPDOSimpleObject {}
--- core/components/buster404/model/buster404/mysql/buster404source.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @package buster404
- */
-require_once (strtr(realpath(dirname(dirname(__FILE__))), '\\', '/') . '/buster404source.class.php');
-class Buster404Source_mysql extends Buster404Source {}
