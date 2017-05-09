5ec7c46d0f7a43a6704c19a28ca60e2507f3bf39
Joeke Kloosterman <joeke@sterc.nl>
2017-05-09 17:09:04 +0200

    Update plugin to add seosuiteurl object when page not found and 404 object not found

--- core/components/seosuite/elements/plugins/seosuite.plugin.php
+++ core/components/seosuite/elements/plugins/seosuite.plugin.php
@@ -3,6 +3,23 @@
  * Plugin for SEO Suite for handling the redirects
  */
 
+$corePath = $modx->getOption(
+    'seosuite.core_path',
+    null,
+    $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/seosuite/'
+);
+$seoSuite = $modx->getService(
+    'seosuite',
+    'SeoSuite',
+    $corePath . 'model/seosuite/',
+    array(
+        'core_path' => $corePath
+    )
+);
+if (!($seoSuite instanceof SeoSuite)) {
+    return;
+}
+
 switch ($modx->event->name) {
     case 'OnPageNotFound':
         $url = $modx->getOption('server_protocol').'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
@@ -17,6 +34,34 @@ switch ($modx->event->name) {
         if ($redirectObject) {
             $redirectUrl = $modx->makeUrl($redirectObject->get('redirect_to'), '', '', 'full');
             $modx->sendRedirect($redirectUrl, 0, 'REDIRECT_HEADER', 'HTTP/1.1 301 Moved Permanently');
+        } else {
+            /* Create new SeoSuiteUrl object, and try to find matches */
+            $suggestions      = '';
+            $redirect_to      = 0;
+            $solved           = 0;
+            $redirect_handler = 0;
+            $findSuggestions  = $seoSuite->findRedirectSuggestions($url);
+            if (count($findSuggestions)) {
+                if (count($findSuggestions) === 1) {
+                    $redirect_to = $findSuggestions[0];
+                    $solved = 1;
+
+                    if (!$seoSuite->checkSeoTab()) {
+                        $redirect_handler = 1;
+                    } else {
+                        $seoSuite->addSeoTabRedirect($url, $findSuggestions[0]);
+                    }
+                }
+                $suggestions = json_encode(array_values($findSuggestions));
+            }
+            $modx->exec(
+                "INSERT INTO {$modx->getTableName('SeoSuiteUrl')}
+                SET {$modx->escape('url')} = {$modx->quote($url)},
+                    {$modx->escape('suggestions')} = {$modx->quote($suggestions)},
+                    {$modx->escape('redirect_to')} = {$modx->quote($redirect_to)},
+                    {$modx->escape('redirect_handler')} = {$modx->quote($redirect_handler)},
+                    {$modx->escape('solved')} = {$modx->quote($solved)}"
+            );
         }
         break;
 }
