8571192b1b25f7c5bf18fb89bacab87e5b3af694
Daniel Havenith <dhh@webaix.de>
2022-10-28 10:09:00 +0200

    added german translation

---
+++ core/components/seosuite/lexicon/de/tab_meta.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * SeoSuite
+ *
+ * Copyright 2019 by Sterc <modx@sterc.com>
+ */
+
+$_lang['seosuite.tab_meta.add_variable']                    = 'Variable hinzufügen';
+$_lang['seosuite.tab_meta.insert_variable']                 = 'Variable hinzufügen';
+$_lang['seosuite.tab_meta.label_variable']                  = 'Variable';
+$_lang['seosuite.tab_meta.label_variable_desc']             = 'Wählen Sie eine Variable, die Sie für Ihr dynamisch erstelltes Meta-Tag verwenden möchten.';
+$_lang['seosuite.tab_meta.submit']                          = 'Hinzufügen';
+$_lang['seosuite.tab_meta.no_preview']                      = 'Suchmaschinen dürfen diese Seite nicht indexieren, daher ist keine Vorschau möglich.';
+
+$_lang['seosuite.tab_meta.use_default']                     = 'Verwenden Sie die Standardeinstellungen für den SEO-Titel und die SEO-Beschreibung.';
+$_lang['seosuite.tab_meta.meta_title']                      = 'SEO Titel';
+$_lang['seosuite.tab_meta.meta_title_desc']                 = 'Erstellen Sie die Vorlage, die für den generierten SEO-Titel verwendet werden soll.';
+$_lang['seosuite.tab_meta.meta_description']                = 'SEO Beschreibung';
+$_lang['seosuite.tab_meta.meta_description_desc']           = 'Erstellen Sie die Vorlage, die für die generierte SEO-Beschreibung verwendet werden soll.';
+$_lang['seosuite.tab_meta.preview']                         = 'Suchmaschine';
+$_lang['seosuite.tab_meta.preview_google']                  = 'Google';
+$_lang['seosuite.tab_meta.preview_yandex']                  = 'Yandex';
+$_lang['seosuite.tab_meta.longtitle']                       = 'Meta-Titel';
+$_lang['seosuite.tab_meta.longtitle_desc']                  = 'Langer Titel. Wenn leer, wird der Standardtitel verwendet.';
+$_lang['seosuite.tab_meta.description']                     = 'Meta-Beschreibung';
+$_lang['seosuite.tab_meta.description_desc']                = '';
+$_lang['seosuite.tab_meta.description_empty']               = 'Fügen Sie eine Beschreibung hinzu. Wenn Sie das nicht tun, versucht die Suchmaschine, einen relevanten Teil der Seite zu finden, der in den Suchergebnissen angezeigt wird.';
+$_lang['seosuite.tab_meta.seo']                             = 'SEO';
+$_lang['seosuite.tab_meta.delimiter']                       = 'Begrenzungszeichen';
+
+/* Keywords. */
+$_lang['seosuite.tab_meta.keywords']                        = 'Keywords für diese Seite';
+$_lang['seosuite.tab_meta.characters.allowed']              = 'Erlaubte Zeichen';
+$_lang['seosuite.tab_meta.keywords']                        = 'Keywords';
+$_lang['seosuite.tab_meta.keywords_desc']                   = 'Geben Sie Ihre Keywords kommagetrennt ein, zum Beispiel: Tisch, Stuhl.';
---
+++ core/components/seosuite/lexicon/de/tab_social.inc.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * SeoSuite
+ *
+ * Copyright 2019 by Sterc <modx@sterc.com>
+ */
+
+$_lang['seosuite.tab_social']                                   = 'Soziale Netzwerke';
+$_lang['seosuite.tab_social.tab_facebook']                      = 'Facebook';
+$_lang['seosuite.tab_social.tab_twitter']                       = 'Twitter';
+
+$_lang['seosuite.tab_social.label_og_title']                    = 'Facebook Titel';
+$_lang['seosuite.tab_social.label_og_title_desc']               = 'Der Seitentitel auf Facebook.';
+$_lang['seosuite.tab_social.label_og_description']              = 'Facebook Beschreibung';
+$_lang['seosuite.tab_social.label_og_description_desc']         = 'Die Seitenbeschreibung auf Facebook.';
+$_lang['seosuite.tab_social.label_og_image']                    = 'Facebook Grafik';
+$_lang['seosuite.tab_social.label_og_image_desc']               = 'Das Seitenbild auf Facebook. Empfohlenes Format ist: 1200 x 630 Pixel.';
+$_lang['seosuite.tab_social.label_og_image_alt']                = 'Facebook-Bildtitel';
+$_lang['seosuite.tab_social.label_og_image_alt_desc']           = 'Der Bildtitel der Seite auf Facebook.';
+$_lang['seosuite.tab_social.label_og_type']                     = 'Facebook Typ';
+$_lang['seosuite.tab_social.label_og_type_desc']                = 'Der Seitentyp auf Facebook.';
+
+$_lang['seosuite.tab_social.label_twitter_title']               = 'Twitter Titel';
+$_lang['seosuite.tab_social.label_twitter_title_desc']          = 'Der Titel der Seite auf Twitter.';
+$_lang['seosuite.tab_social.label_twitter_description']         = 'Twitter Beschreibung';
+$_lang['seosuite.tab_social.label_twitter_description_desc']    = 'Die Beschreibung der Seite auf Twitter.';
+$_lang['seosuite.tab_social.label_twitter_image']               = 'Twitter Grafik';
+$_lang['seosuite.tab_social.label_twitter_image_desc']          = 'Das Seitenbild auf Twitter. Empfohlenes Format ist: 1200 x 630 Pixel.';
+$_lang['seosuite.tab_social.label_twitter_image_alt']           = 'Twitter Bildtitel';
+$_lang['seosuite.tab_social.label_twitter_image_alt_desc']      = 'Der Bildtitel der Seite auf Twitter.';
+$_lang['seosuite.tab_social.label_twitter_creator_id']          = 'Ersteller/Autor von Twitter-Inhalten';
+$_lang['seosuite.tab_social.label_twitter_creator_id_desc']     = '@Benutzername für den Ersteller/Autor des Inhalts.';
+$_lang['seosuite.tab_social.label_twitter_card']                = 'Twitter Card';
+$_lang['seosuite.tab_social.label_twitter_card_desc']           = 'The page card on Twitter.';
+$_lang['seosuite.tab_social.label_inherit_facebook']            = 'Facebook-Werte übernehmen';
+$_lang['seosuite.tab_social.label_inherit_facebook_desc']       = 'Ja';
