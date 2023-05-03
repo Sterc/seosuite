b3efa962da887383ea2da8cfd08d84c19935d5a4
Ryan Thrash <ryan@thrash.me>
2023-05-03 16:26:33 -0500

    Update Facebook to Open Graph

--- core/components/seosuite/lexicon/en/tab_social.inc.php
+++ core/components/seosuite/lexicon/en/tab_social.inc.php
@@ -6,20 +6,21 @@
  * Copyright 2019 by Sterc <modx@sterc.com>
  */
 
-$_lang['seosuite.tab_social']                                   = 'Social';
-$_lang['seosuite.tab_social.tab_facebook']                      = 'Facebook';
+$_lang['seosuite.tab_social']                                   = 'Sharing';
+$_lang['seosuite.tab_social.tab_facebook']                      = 'Open Graph';
 $_lang['seosuite.tab_social.tab_twitter']                       = 'Twitter';
 
-$_lang['seosuite.tab_social.label_og_title']                    = 'Facebook title';
-$_lang['seosuite.tab_social.label_og_title_desc']               = 'The title of the page on Facebook.';
-$_lang['seosuite.tab_social.label_og_description']              = 'Facebook description';
-$_lang['seosuite.tab_social.label_og_description_desc']         = 'The description of the page on Facebook.';
-$_lang['seosuite.tab_social.label_og_image']                    = 'Facebook image';
-$_lang['seosuite.tab_social.label_og_image_desc']               = 'The image of the page on Facebook, the beste format is 1200 x 630 pixels.';
-$_lang['seosuite.tab_social.label_og_image_alt']                = 'Facebook image title';
-$_lang['seosuite.tab_social.label_og_image_alt_desc']           = 'The image title of the page on Facebook.';
-$_lang['seosuite.tab_social.label_og_type']                     = 'Facebook type';
-$_lang['seosuite.tab_social.label_og_type_desc']                = 'The page type on Facebook.';
+$_lang['seosuite.tab_social.label_og_title']                    = 'Title (og:title)';
+$_lang['seosuite.tab_social.label_og_title_desc']               = 'REQUIRED: The title of the page as it should appear within the graph, e.g., for Facebook shares.';
+$_lang['seosuite.tab_social.label_og_description']              = 'Description (og:description)';
+$_lang['seosuite.tab_social.label_og_description_desc']         = 'The page description of the page on Facebook.';
+$_lang['seosuite.tab_social.label_og_image']                    = 'Image (og:image)';
+$_lang['seosuite.tab_social.label_og_image_desc']               = 'REQUIRED: An image URL which should represent your object within the graph. The best format is 1200 x 630 pixels.';
+$_lang['seosuite.tab_social.label_og_image_alt']                = 'Image alt title (og:image:alt)';
+$_lang['seosuite.tab_social.label_og_image_alt_desc']           = 'A description of what is in the image (not a caption).';
+$_lang['seosuite.tab_social.label_og_type']                     = 'Type (og:type)';
+$_lang['seosuite.tab_social.label_og_type_desc']                = 'REQUIRED: The type of your object, e.g., “website”. Depending on the type you specify, other properties may also be required.
+.';
 
 $_lang['seosuite.tab_social.label_twitter_title']               = 'Twitter title';
 $_lang['seosuite.tab_social.label_twitter_title_desc']          = 'The title of the page on Twitter.';
